1badf991523539ff501f26f367b38e621ce25c01
Eric <github@esellors.com>
2019-06-05 17:35:00 -0500

    delete rows complete

--- index.sql
+++ index.sql
@@ -95,6 +95,35 @@
 -- JOIN artist ON artist.artist_id = album.artist_id
 -- GROUP BY artist.name;
 
+--USE DISTINCT
+--#1
+-- SELECT DISTINCT composer FROM track;
+--#2
+-- SELECT DISTINCT billing_postal_code FROM invoice;
+--#3
+-- SELECT DISTINCT company FROM customer;
+
+--DELETE ROWS
+-- CREATE TABLE practice_delete ( name TEXT, type TEXT, value INTEGER );
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'bronze', 50);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'bronze', 50);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'bronze', 50);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'silver', 100);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'silver', 100);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+-- INSERT INTO practice_delete ( name, type, value ) VALUES ('delete', 'gold', 150);
+--#1
+-- DELETE FROM practice_delete
+-- WHERE type = 'bronze';
+--#2
+-- DELETE FROM practice_delete
+-- WHERE type = 'silver';
+---#3
+-- DELETE FROM practice_delete 
+-- WHERE value = 150;
+
 
 
 
