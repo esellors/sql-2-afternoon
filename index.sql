e7d9cbff29150abffbbd815c7cb4f4f3ceb51263
Eric <github@esellors.com>
2019-06-05 16:02:03 -0500

    practice nested queries complete

--- index.sql
+++ index.sql
@@ -29,3 +29,39 @@
 -- JOIN album a ON t.album_id = a.album_id
 -- JOIN genre g ON g.genre_id = t.genre_id
 -- WHERE g.name = 'Alternative & Punk';
+
+--PRACTICE NESTED QUERIES
+--#1
+-- SELECT * FROM invoice
+-- WHERE invoice_id IN (SELECT invoice_id FROM invoice_line WHERE unit_price > 0.99);
+--#2
+-- SELECT * FROM playlist_track
+-- WHERE playlist_id IN ( SELECT playlist_id FROM playlist WHERE name = 'Music' );
+--#3
+-- SELECT name FROM track
+-- WHERE track_id IN (SELECT track_id FROM playlist_track WHERE playlist_id = 5);
+--#4
+-- SELECT * FROM track
+-- WHERE genre_id IN (SELECT genre_id FROM genre WHERE name = 'Comedy');
+--#5
+-- SELECT * FROM track
+-- WHERE album_id IN (SELECT album_id FROM album WHERE title = 'Fireball');
+--#6
+-- SELECT * FROM track
+-- WHERE album_id IN (
+--   SELECT album_id FROM album WHERE artist_id IN (
+--     SELECT artist_id FROM artist WHERE name = 'Queen'
+--   )
+-- );
+  
+
+
+
+
+
+
+
+
+
+
+
