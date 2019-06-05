9487467631ac98ca835326457b1c6d0365d646c8
Eric <github@esellors.com>
2019-06-05 16:35:03 -0500

    practice updating rows complete

--- index.sql
+++ index.sql
@@ -54,9 +54,30 @@
 --   )
 -- );
   
+--PRACTICE UPDATING ROWS
+--#1
+-- UPDATE customer
+-- SET fax = NULL
+-- WHERE fax IS NOT NULL;
+--#2
+-- UPDATE customer
+-- SET company = 'Self'
+-- WHERE company IS NULL;
+--#3
+-- UPDATE customer
+-- SET last_name = 'Thompson'
+-- WHERE customer_id = 28;
+--#4
+-- UPDATE customer
+-- SET support_rep_id = 4
+-- WHERE email = 'luisrojas@yahoo.cl';
+--#5
+-- UPDATE track
+-- SET composer = 'The darkness around us'
+-- WHERE genre_id = (SELECT genre_id FROM genre WHERE name = 'Metal') 
+-- AND composer IS NULL;
 
-
-
+               
 
 
 
